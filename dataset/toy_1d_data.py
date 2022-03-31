bd6edd140e732f89065834534ffab76bf4206394
Howard <suyihao1999@gmail.com>
2022-03-31 07:06:50 +0000

    add 1d dataset

---
+++ dataset/toy_1d_data.py
@@ -0,0 +1,29 @@
+import numpy as np
+from numpy.random import uniform, randn
+
+
+def load_wiggle():
+
+    np.random.seed(0)
+    Npoints = 300
+    x = randn(Npoints) * 2.5 + 5  # uniform(0, 10, size=Npoints)
+
+    def function(x):
+        return np.sin(np.pi * x) + 0.2 * np.cos(np.pi * x * 4) - 0.3 * x
+
+    y = function(x)
+
+    homo_noise_std = 0.25
+    homo_noise = randn(*x.shape) * homo_noise_std
+    y = y + homo_noise
+
+    x = x[:, None]
+    y = y[:, None]
+
+    x_means, x_stds = x.mean(axis=0), x.std(axis=0)
+    y_means, y_stds = y.mean(axis=0), y.std(axis=0)
+
+    X = ((x - x_means) / x_stds).astype(np.float32)
+    Y = ((y - y_means) / y_stds).astype(np.float32)
+
+    return X, Y
